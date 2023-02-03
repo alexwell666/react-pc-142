3445d27b2fda5e4b14fa25e01bb58f50c553a651
alexwell666 <wrc2021072601@gmail.com>
2023-02-03 20:52:23 +0800

    login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,27 +4,29 @@ import './index.scss'
 
 import { Form, Input, Button, Checkbox } from 'antd'
 
-
-
 function Login () {
   return (
     <div className='login'>
       <Card className='login-container'>
         <img className='login-logo' src={logo} alt="" />
-        <Form>
+        <Form
+          initialValues={{
+            remember: true
+          }}
+        >
           <Form.Item>
             <Input size="large" placeholder="请输入手机号"></Input>
           </Form.Item>
           <Form.Item>
             <Input size="large" placeholder="请输入验证码"></Input>
           </Form.Item>
-          <Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
             <Checkbox className="login-checkbox-label">
               我已阅读并同意「用户协议」和「隐私条款」
             </Checkbox>
           </Form.Item>
           <Form.Item>
-            <Button type="primary" htmlType="submit">
+            <Button type="primary" htmlType="submit" size="large" block>
               登陆
             </Button>
           </Form.Item>
